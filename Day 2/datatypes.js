bbfd8ed6176ea79f09abe5d643594a4eaa02fb98
Priyanshu <pri2712gumber@gmail.com>
2024-04-12 01:19:03 +0530

    Datatyes

---
+++ Day 2/datatypes.js
@@ -0,0 +1,25 @@
+"use strictt"; //use javascript as a newer version
+
+// alert(3 + 3); // We are using node js so it will give a error,in browser it will give a popup
+
+console.log(3 
+    +
+      3)// dont use extra space its bad for code readabiltiy
+
+console.log("Hitesh")
+
+let name = "Hitesh" //String
+let age = 18 // number 
+let islogin = true // Bool
+
+// number  ==> 2 to the power 53
+// bigint 
+// string ++> ""
+// boolean ==> true/false
+// null ==> standalone value 
+// undefined ==> abhi value assign nhi hui hai
+// symbol ==> unique 
+
+//object 
+
+console.log(typeof undefined);
