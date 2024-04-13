100aa95be9868da8406e2c8cedac44de1de16140
Priyanshu <pri2712gumber@gmail.com>
2024-04-13 16:25:21 +0530

    operations created

---
+++ Day3/operations.js
@@ -0,0 +1,35 @@
+// let value = 3
+// let negval = -value
+
+// console.log(negval);    
+/*  console.log(2+2);    
+    console.log(2-2);    
+    console.log(2**3);    
+    console.log(2*2);    
+    console.log(2/2);    
+    console.log(2%2);    */
+
+// let str1 = "hello"
+// let str2 = " priyanshu"
+
+// let str3 = str1 + str2 
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3 + 4 )* 5 % 3);
+
+// console.log(+true); 
+// console.log(+""); 
+
+let counter = 100
+// counter++;
+++counter;
+console.log(counter);
+
+// Read mdn prefix and postfix 
